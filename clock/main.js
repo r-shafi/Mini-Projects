893d82ab1b341fa8c1a6d8a37a28e2c573d29d05
Shafi Rayhan <rayhanshafi7@gmail.com>
2020-12-12 12:18:22 +0600

    theme unified

--- clock/main.js
+++ clock/main.js
@@ -5,12 +5,10 @@ let format = '24H';
 
 const radioButtons = document.querySelectorAll('[type="radio"]');
 
-radioButtons.forEach((btn) =>
-  btn.addEventListener('click', (e) => (format = e.target.value))
-);
+radioButtons.forEach((btn) => btn.addEventListener('click', (e) => (format = e.target.value)));
 
 dateBody.innerHTML = Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
-  new Date()
+  new Date(),
 );
 
 function timeUpdater() {
@@ -19,14 +17,14 @@ function timeUpdater() {
   const min = now.getMinutes();
   const sec = now.getSeconds();
 
-  timeBody.innerHTML =
-    format === '24H'
-      ? `${hour < 10 ? `0${hour}` : hour}:${min < 10 ? `0${min}` : min}:${
-          sec < 10 ? `0${sec}` : sec
-        }`
-      : `${hour - 12 < 10 ? `0${hour - 12}` : hour - 12}:${
-          min < 10 ? `0${min}` : min
-        }:${sec < 10 ? `0${sec}` : sec}<span>${hour < 12 ? 'am' : 'pm'}</span>`;
+  timeBody.innerHTML = format === '24H'
+    ? `${hour < 10 ? `0${hour}` : hour}:${min < 10 ? `0${min}` : min}:${
+      sec < 10 ? `0${sec}` : sec
+    }`
+    // abc divider
+    : `${hour <= 12 ? hour === 11 ? hour : hour === 12 ? hour : `0${hour}` : hour - 12}:${
+      min < 10 ? `0${min}` : min
+    }:${sec < 10 ? `0${sec}` : sec}<span>${hour < 12 ? 'am' : 'pm'}</span>`;
 }
 
 setInterval(timeUpdater, 1000);
